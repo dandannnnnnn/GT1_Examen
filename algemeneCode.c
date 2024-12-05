c81054703ac80997cc157ef6149b70051dc477c4
dandannnnnnn <dandansandan04@gmail.com>
2024-12-05 19:15:32 +0100

    preadding messageHandler func and main func

--- algemeneCode.c
+++ algemeneCode.c
@@ -15,8 +15,34 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <MQTTClient.h>
+#include <MQTTClient.h> //library for MQTT functions
+
+#define IP_ADDRESS "tcp://192.168.0.5:1883" //IP-ADDRESS:port
+#define CLIENT ""
+#define PAYLOAD "Hello from RPi 101!"
+#define topic "MB_subscribe" //subscribing to MQTT to receive message
+#define TIMEOUT 10000L
+
+void messageHandler(void *context, char *topicName, int topicLEN, MQTTClient_message *message) {
+    FILE *file;
+
+    file = fopen("receivedMSGs.txt", "a");
+    if (file != NULL) {
+        fprintf(file, "Topic: %s\tMessage: %.*s\n", topicName, message -> payloadlen, (char *)message->payload);
+        fclose(file;)
+    } else {
+        printf("Error: cannot open file");
+    }
+
+    MQTTClient_freeMessage(&message);
+    MQTTClient_free(topicName);
+
+}
 
 int main() {
-    return 0;
+    MQTTClient CLIENT;
+    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+    int rc;
+
+    
 }
